fc78140dbea4e694634682b97df33961998419b2
Troy Unverdruss <troy@unverdruss.net>
2020-12-07 09:04:22 -0800

    well that was 'fun', day 7 done, kind of useless algorithm ha

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 
 #[allow(unused_imports)]
 use itertools::Itertools;
+#[allow(unused_imports)]
 use std::time::Instant;
 
 mod day01;
@@ -16,7 +17,7 @@ mod day07;
 mod util;
 
 fn main() {
-    let _day = 6;
+    let _day = 7;
 
     let mut days: HashMap<i32, fn()> = HashMap::new();
     days.insert(1, day01::run);
@@ -27,16 +28,16 @@ fn main() {
     days.insert(6, day06::run);
     days.insert(7, day07::run);
 
-    // if let Some(f) = days.get(&day) {
-    //     f()
-    // };
+    if let Some(f) = days.get(&_day) {
+        f()
+    };
 
     // Run all the days in order
-    days.iter().sorted().for_each(|e| {
-        println!("Day {}", e.0);
-        let now = Instant::now();
-        e.1();
-        println!("Elapsed: {}", now.elapsed().as_millis());
-        println!();
-    })
+    // days.iter().sorted().for_each(|e| {
+    //     println!("Day {}", e.0);
+    //     let now = Instant::now();
+    //     e.1();
+    //     println!("Elapsed: {}", now.elapsed().as_millis());
+    //     println!();
+    // })
 }
