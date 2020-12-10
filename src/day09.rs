d402e2b885bcb9915a00e0796bbb586e023f952d
Troy Unverdruss <troy@unverdruss.net>
2020-12-10 09:01:36 -0800

    up to day 10

---
+++ src/day09.rs
@@ -0,0 +1,120 @@
+use crate::util::inputs::day_input;
+use itertools::Itertools;
+use std::collections::HashSet;
+
+pub fn run() {
+    let numbers = day_input(9)
+        .iter()
+        .map(|s| s.parse::<i64>().expect("Could not parse int"))
+        .collect::<Vec<i64>>();
+
+    let first_invalid_number = part1(&numbers, 25);
+    println!("Part 1: first invalid number: {}", first_invalid_number);
+
+    let encryption_weakness = part2(&numbers, first_invalid_number);
+}
+
+fn part1(numbers: &Vec<i64>, preamble: usize) -> i64 {
+    for i in preamble..numbers.len() {
+        let mut pre = vec![0; preamble];
+        pre.clone_from_slice(&numbers[i - preamble..i]);
+
+        println!("slice: {:?}", pre);
+
+        let target = numbers[i];
+        println!("target: {:?}", target);
+
+        let candidates = pre
+            .iter()
+            .filter(|n| *n < &target)
+            .map(|n| n.clone())
+            .collect::<Vec<i64>>();
+
+
+        println!("candidates: {:?}", candidates);
+
+        let valid = candidates
+            .iter()
+            .any(|c| {
+                let pair = target - c;
+                let res = if pair == *c {
+                    false
+                } else if candidates.contains(&pair) {
+                    true
+                } else {
+                    false
+                };
+                println!("{}+{}={} valid: {}", c, pair, target, res);
+                res
+            });
+
+        if !valid {
+            return numbers[i];
+        }
+    }
+    panic!("Never found a suitable number")
+}
+
+fn part2(numbers: &Vec<i64>, target_invalid_number: i64) -> i64 {
+  let mut running_sum = 0;
+    let sums_in_position = numbers.iter().map(|n| {
+        running_sum += n;
+        running_sum
+    }).collect::<Vec<i64>>();
+
+    println!("Target number: {}", target_invalid_number);
+    println!("Part 2 numbers    : {:?}", numbers);
+    println!("Part 2 number sums: {:?}", sums_in_position);
+
+    for end in 0..sums_in_position.len() {
+        for start in 0..end {
+            if sums_in_position[end] - sums_in_position[start] == target_invalid_number {
+                println!("Index {}..{}", start, end);
+                println!("Sum {}..{}", numbers[end], numbers[start+1]);
+
+                let min = numbers[start+1..end].iter().min().unwrap();
+                let max = numbers[start+1..end].iter().max().unwrap();
+
+                let sum = min + max;
+                println!("Sum: {}", sum);
+                return sum
+            }
+        }
+    }
+
+    panic!("Never found a valid number!")
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day09::{part1, part2};
+
+    #[test]
+    fn test_input() {
+        let numbers = vec![
+            35,
+        20,
+        15,
+        25,
+        47,
+        40,
+        62,
+        55,
+        65,
+        95,
+        102,
+        117,
+        150,
+        182,
+        127,
+        219,
+        299,
+        277,
+        309,
+        576
+        ];
+
+        assert_eq!(127, part1(&numbers, 5));
+        assert_eq!(62, part2(&numbers, 127));
+    }
+}
---
+++ src/day10.rs
@@ -0,0 +1,131 @@
+use crate::util::inputs::day_input;
+use itertools::Itertools;
+use std::collections::HashMap;
+use std::error::Error;
+use std::io;
+
+pub fn run() {
+    let mut adapters = day_input(10)
+        .iter()
+        .map(|s| s.parse::<i32>().unwrap())
+        .sorted()
+        .collect::<Vec<i32>>();
+
+    let part1 = part_1(&adapters);
+    println!("Part 1: {}", part1);
+
+    let part2 = part_2(&adapters);
+    println!("Part 2: {}", part2);
+}
+
+fn part_1(adapters: &Vec<i32>) -> i32 {
+    let mut results = HashMap::new();
+    results.insert(1, 0);
+    results.insert(2, 0);
+    // 3 starts at 1 because we always have a 3 at the end to get to our device
+    results.insert(3, 1);
+
+    let mut last_val = 0;
+    for a in adapters {
+        let change = *a - last_val;
+        match change {
+            1 => results.entry(1).and_modify(|f| *f += 1),
+            2 => results.entry(2).and_modify(|f| *f += 1),
+            3 => results.entry(3).and_modify(|f| *f += 1),
+            _ => {
+                let error = format!("{} adapter greater than 3 from {}", a, last_val);
+                panic!(error);
+            }
+        };
+        last_val = *a;
+    }
+    let ones = results.get(&1).unwrap();
+    let threes = results.get(&3).unwrap();
+
+    ones * threes
+}
+
+fn part_2(adapters: &Vec<i32>) -> i64 {
+    let mut memo = HashMap::new();
+    solver(&0, &adapters[..], &mut memo).unwrap()
+}
+
+fn solver(
+    current: &i32,
+    remaining: &[i32],
+    memo: &mut HashMap<String, Option<i64>>,
+) -> Option<i64> {
+    let key = remaining.iter().join("-");
+    if let Some(key) = memo.get(&key) {
+        if let Some(value) = key {
+            return Some(*value);
+        }
+    }
+
+    let result = if remaining.is_empty() {
+        Some(1)
+    } else {
+        let next_steps: Vec<i32> = remaining.iter().map(|v| *v).filter(|v| *v - current <= 3).collect();
+        assert!(next_steps.len() <= 3);
+
+        if next_steps.len() == 0 {
+            None
+        } else {
+            let mut sum = 0;
+            for (index, value) in next_steps.iter().enumerate() {
+                let result = solver(value, &remaining[index+1..], memo);
+                if let Some(val) = result {
+                    sum += val;
+                }
+            }
+            if sum == 0 {
+                None
+            } else {
+                Some(sum)
+            }
+        }
+
+    };
+
+    memo.insert(key.to_owned(), result.to_owned());
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day10::{part_1, part_2};
+
+    #[test]
+    fn example_1_1() {
+        let mut adapters = vec![16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4];
+        adapters.sort();
+        assert_eq!(35, part_1(&adapters))
+    }
+
+    #[test]
+    fn example_1_2() {
+        let mut adapters = vec![
+            28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35,
+            8, 17, 7, 9, 4, 2, 34, 10, 3,
+        ];
+        adapters.sort();
+        assert_eq!(220, part_1(&adapters))
+    }
+
+    #[test]
+    fn example_2_1() {
+        let mut adapters = vec![16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4];
+        adapters.sort();
+        assert_eq!(8, part_2(&adapters))
+    }
+
+    #[test]
+    fn example_2_2() {
+        let mut adapters = vec![
+            28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35,
+            8, 17, 7, 9, 4, 2, 34, 10, 3,
+        ];
+        adapters.sort();
+        assert_eq!(19208, part_2(&adapters))
+    }
+}
