0e3215f2940ce12a9f1bfbf32a264855d0239751
Troy Unverdruss <troy@unverdruss.net>
2020-12-21 22:30:01 -0500

    halfway through part 2 of day 20

--- src/day20.rs
+++ src/day20.rs
@@ -17,36 +17,91 @@ pub fn run() {
         lookup.insert(t.id, t.clone());
     }
 
+    let mut tiles_to_edges: HashMap<i64, Vec<EdgeMatch>> = HashMap::new();
     let mut part1_product = 1;
+    let mut sum = 0;
     let ids = tiles.iter().map(|t| t.id).collect::<Vec<i64>>();
     for id in &ids {
         let mut count = 0;
         for edge in &lookup.get(id).unwrap().edges {
-            for id2 in &ids {
-                if id == id2 {
+            for other_id in &ids {
+                if id == other_id {
                     continue;
                 }
-                let other_tile = lookup.get(id2).unwrap();
+                let other_tile = lookup.get(other_id).unwrap();
                 if other_tile.edges.contains(edge) {
-                    count += 1
+                    count += 1;
+                    let e = tiles_to_edges.entry(*id).or_insert(Vec::new());
+                    e.push(EdgeMatch {
+                        other_id: *other_id,
+                        orientation: Orientation::Default,
+                    })
                 } else if other_tile.edges_flip_x.contains(edge) {
-                    count += 1
+                    count += 1;
+                    let e = tiles_to_edges.entry(*id).or_insert(Vec::new());
+                    e.push(EdgeMatch {
+                        other_id: *other_id,
+                        orientation: Orientation::FlipX,
+                    })
                 } else if other_tile.edged_flip_y.contains(edge) {
-                    count += 1
+                    count += 1;
+                    let e = tiles_to_edges.entry(*id).or_insert(Vec::new());
+                    e.push(EdgeMatch {
+                        other_id: *other_id,
+                        orientation: Orientation::FlipY,
+                    })
                 } else if other_tile.edges_flip_both.contains(edge) {
-                    count += 1
+                    count += 1;
+                    let e = tiles_to_edges.entry(*id).or_insert(Vec::new());
+                    e.push(EdgeMatch {
+                        other_id: *other_id,
+                        orientation: Orientation::FlipBoth,
+                    })
                 } else {
-                    count += 0
+                    count += 0;
+                    // perimeter edge
                 }
             }
-
         }
 
         if count == 2 {
             part1_product *= id
         }
+        sum += count;
     }
-    println!("Part 1: {}", part1_product)
+    println!("Part 1: {}", part1_product);
+    println!("  sums: {}", sum);
+
+    let start = tiles_to_edges
+        .iter()
+        .filter(|e| e.1.len() == 2)
+        .next()
+        .unwrap()
+        .0;
+
+    let mut map: HashMap<Point, MapPosition> = HashMap::new();
+
+    for y in 0..12 {
+        for x in 0..12 {
+            let here = Point::new(x, y);
+            // Get this thing started
+            if x == 0 && y == 0 {
+                map.insert(
+                    Point::new(0, 0),
+                    MapPosition {
+                        id: *start,
+                        orientation: Orientation::Default,
+                    },
+                );
+            } else if x == 0 {
+                // Otherwise look up if we're at the start of the row
+                let up_tile = map.get(&Point::new(x, y - 1)).unwrap();
+            } else {
+                // lastly look left if we aren't at the start of a row
+                let left_tile = map.get(&Point::new(x - 1, y)).unwrap();
+            }
+        }
+    }
 }
 
 #[derive(PartialEq, Eq, Debug, Clone)]
@@ -57,6 +112,68 @@ struct Tile {
     edges_flip_x: HashSet<String>,
     edged_flip_y: HashSet<String>,
     edges_flip_both: HashSet<String>,
+    top: String,
+    bottom: String,
+    left: String,
+    right: String,
+}
+
+impl Tile {
+    fn top(&self, orientation: Orientation) -> String {
+        match orientation {
+            Orientation::Default => self.top.clone(),
+            Orientation::FlipX => self.bottom.clone(),
+            Orientation::FlipY => self.top.clone(),
+            Orientation::FlipBoth => self.bottom.clone(),
+        }
+    }
+
+    fn bottom(&self, orientation: Orientation) -> String {
+        match orientation {
+            Orientation::Default => self.bottom.clone(),
+            Orientation::FlipX => self.top.clone(),
+            Orientation::FlipY => self.bottom.clone(),
+            Orientation::FlipBoth => self.top.clone(),
+        }
+    }
+
+    fn left(&self, orientation: Orientation) -> String {
+        match orientation {
+            Orientation::Default => self.left.clone(),
+            Orientation::FlipX => self.left.clone(),
+            Orientation::FlipY => self.right.clone(),
+            Orientation::FlipBoth => self.right.clone(),
+        }
+    }
+
+    fn right(&self, orientation: Orientation) -> String {
+        match orientation {
+            Orientation::Default => self.right.clone(),
+            Orientation::FlipX => self.right.clone(),
+            Orientation::FlipY => self.left.clone(),
+            Orientation::FlipBoth => self.left.clone(),
+        }
+    }
+}
+
+#[derive(PartialEq, Eq, Debug, Clone)]
+enum Orientation {
+    Default,
+    FlipX,
+    FlipY,
+    FlipBoth,
+}
+
+#[derive(PartialEq, Eq, Debug, Clone)]
+struct EdgeMatch {
+    other_id: i64,
+    orientation: Orientation,
+}
+
+#[derive(PartialEq, Eq, Debug, Clone)]
+struct MapPosition {
+    id: i64,
+    orientation: Orientation,
 }
 
 fn parse_tile(lines: Vec<String>) -> Tile {
@@ -69,7 +186,11 @@ fn parse_tile(lines: Vec<String>) -> Tile {
         .get(1)
         .map_or(-1, |m| String::from(m.as_str()).parse::<i64>().unwrap());
 
-    let grid = lines[1..].iter().filter(|l| !l.is_empty()).map(String::from).collect::<Vec<String>>();
+    let grid = lines[1..]
+        .iter()
+        .filter(|l| !l.is_empty())
+        .map(String::from)
+        .collect::<Vec<String>>();
     let grid_data = GridData::parse_input(grid);
 
     let mut edges: HashSet<String> = HashSet::new();
