fad2a65781ac389f75d6636134d87b526bdb409d
Troy Unverdruss <troy@unverdruss.net>
2020-12-26 09:19:28 -0500

    rewrite of the algorithm for speed, part 2 done

--- src/day23.rs
+++ src/day23.rs
@@ -4,89 +4,105 @@ use std::iter::FromIterator;
 
 pub fn run() {
     let input = "167248359";
-    let result = part1(input);
-    println!("Part 1: {}" , result);
+    let result = part2(input, 9, 100);
+    println!("Part 1: {}", result.part1);
+    let result = part2(input, 1_000_000, 10_000_000);
+    println!("Part 2: {}", result.part2);
 }
 
-fn part1(input: &str) -> String {
-    let all_cups: Vec<usize> = Vec::from_iter(
+fn part2(input: &str, highest_cup: usize, iterations: usize) -> GameResult {
+    let mut cups : Vec<i64> = vec![0; highest_cup + 1];
+    let input_cups: Vec<i64> = Vec::from_iter(
         input
             .chars()
-            .map(|c| String::from(c).parse::<usize>().unwrap()),
+            .map(|c| String::from(c).parse::<i64>().unwrap()),
     );
-    let mut next_cup_order = all_cups
-        .iter()
-        .sorted()
-        .rev()
-        .copied()
-        .collect::<VecDeque<usize>>();
 
-    let mut cups = VecDeque::from_iter(all_cups);
+    let start = *input_cups.get(0).unwrap();
 
-    let mut step = 0;
-    for step in 0..100 {
-        println!("Step {}: cups: {:?}", step, cups);
-        let current_cup = cups.front().unwrap().clone();
-        cups.rotate_left(1);
-        let a = cups.pop_front().unwrap();
-        let b = cups.pop_front().unwrap();
-        let c = cups.pop_front().unwrap();
+    let mut cup_to_set = start;
 
-        loop {
-            if next_cup_order.back().unwrap() == cups.back().unwrap() {
-                break;
-            }
-            next_cup_order.rotate_left(1);
-        }
-        loop {
-            if !vec![a, b, c].contains(next_cup_order.front().unwrap()) {
-                break;
-            }
-            next_cup_order.rotate_left(1);
-        }
+    for i in 1..input_cups.len() {
+        let next = *input_cups.get(i).unwrap();
+        cups[cup_to_set as usize] = next;
+        cup_to_set = next as i64;
+    }
 
-        loop {
-            if cups.back().unwrap() == next_cup_order.front().unwrap() {
-                break;
-            }
-            cups.rotate_left(1);
-        }
-        cups.push_front(c);
-        cups.push_front(b);
-        cups.push_front(a);
-
-        loop {
-            if *cups.back().unwrap() == current_cup {
-                break
-            }
-            cups.rotate_left(1);
+    if highest_cup > 9 {
+        for i in 10..=highest_cup {
+            cups[cup_to_set as usize] = i as i64;
+            cup_to_set = i as i64;
         }
     }
 
-    loop {
-        if *cups.back().unwrap() == 1 {
-            break
-        }
-        cups.rotate_left(1);
+    cups[cup_to_set as usize] = start;
+
+    // ok cups are "set up", time to start shuffling these things
+
+    let mut start_n = start;
+    for _ in 0..iterations {
+        let a = cups[start_n as usize];
+        let b = cups[a as usize];
+        let c = cups[b as usize];
+        let next_start_d = cups[c as usize];
+
+        cups[start_n as usize] = next_start_d;
+        let insertion_point_e = {
+            let mut next = start_n;
+            loop {
+                if next == 1 {
+                    next = highest_cup as i64
+                } else {
+                    next -= 1
+                };
+
+                if next != a && next != b && next != c {
+                    break;
+                }
+            }
+            next
+        };
+        let next_cup_after_insertion_f = cups[insertion_point_e as usize];
+        cups[insertion_point_e as usize] = a;
+        cups[c as usize ] = next_cup_after_insertion_f;
+        start_n = next_start_d;
     }
 
-    let mut result = vec![];
-    loop  {
-        if *cups.front().unwrap() == 1 {
-            break
-        }
-        result.push(*cups.front().unwrap());
-        cups.rotate_left(1);
+    // Get part 1 result
+    let mut part1_result = vec![];
+    let mut next = 1;
+    for _ in 0..8 {
+        next = cups[next as usize];
+        part1_result.push(next);
     }
+    let part1_result = part1_result.iter().map(|v| format!("{}", v)).join("");
 
-    result.iter().map(|v| format!("{}", v)).join("")
+    // Get part 2 result
+    let part2_a = cups[1];
+    let part2_b = cups[part2_a as usize];
+    let part2_result = part2_a * part2_b;
+
+    GameResult {
+        part1: part1_result,
+        part2: part2_result
+    }
+}
+
+struct GameResult {
+    part1: String,
+    part2: i64,
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day23::part1;
+    use crate::day23::part2;
     #[test]
-    fn example1() {
-        assert_eq!("67384529".to_owned(), part1(&"389125467"));
+    fn part1_example() {
+        assert_eq!("67384529".to_owned(), part2(&"389125467", 9, 100).part1);
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(149245887792, part2(&"389125467", 1_000_000, 10_000_000).part2);
     }
 }
